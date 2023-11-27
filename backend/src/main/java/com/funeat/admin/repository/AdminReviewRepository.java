8c6fd4e5288c86ba12ae4e8eb27335cb1c18079b
Leejin Yang <ejin1996@gmail.com>
2023-11-27 18:12:13 +0900

    chore: 클라이언트 레포 이관 (#2)
    * chore: 클라이언트 폴더 제외 모든 파일 삭제
    
    * chore: tsconfig baseUrl 수정
    
    * style: index import 경로 수정
    
    * chore: 이슈 템플릿 제목 prefix 제거
    
    * fix: workflow working directory 수정

--- backend/src/main/java/com/funeat/admin/repository/AdminReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.funeat.admin.repository;
-
-import com.funeat.common.repository.BaseRepository;
-import com.funeat.review.domain.Review;
-
-public interface AdminReviewRepository extends BaseRepository<Review, Long> {
-}
