0a15c1722d9129440fe7f24f20a6ffbc92fdacaa
TaeeunKim <xodms0309@naver.com>
2024-01-05 17:41:47 +0900

    feat: 프로필 이미지에 오류 발생 시 기본 이미지로 대체

--- src/components/Members/MemberImage/MemberImage.tsx
+++ src/components/Members/MemberImage/MemberImage.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import type { CSSProp } from 'styled-components';
 import styled from 'styled-components';
+
+import DefaultMemberImage from '@/assets/defaultProfile.png';
 
 interface MemberImageProps {
   src: string;
   alt: string;
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
   css?: CSSProp;
 }
 
-const MemberImage = ({ src, alt, width, height, css }: MemberImageProps) => {
-  return <StyledMemberImage src={src} alt={alt} width={width} height={height} css={css} />;
+const MemberImage = ({ src, alt, width = 45, height = 45, css }: MemberImageProps) => {
+  const [imageSrc, setImageSrc] = useState(src);
+
+  const handleImageError = () => {
+    setImageSrc(DefaultMemberImage);
+  };
+
+  return (
+    <StyledMemberImage src={imageSrc} alt={alt} width={width} height={height} css={css} onError={handleImageError} />
+  );
 };
 
 export default MemberImage;
@@ -19,5 +30,6 @@ const StyledMemberImage = styled.img`
   border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 50%;
   background: ${({ theme }) => theme.backgroundColors.default};
+  object-fit: cover;
   ${({ css }) => css};
 `;
