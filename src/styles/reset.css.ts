5faa02ccb619cec482a72824cf985c7b206a520f
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2024-03-06 22:57:23 +0900

    Vanilla extract 기본 셋팅 (#23)
    * chore: vanilla extract 설치
    
    * chore: vanilla extract 환경 설정
    
    * refactor: 글로벌 스타일 vanilla extract로 마이그레이션

---
+++ src/styles/reset.css.ts
@@ -0,0 +1,10 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('#root', {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
+});
