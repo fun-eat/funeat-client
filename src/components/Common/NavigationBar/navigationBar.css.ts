aa388fcb869b09f1461d43e7e7b5f1519a767fad
Leejin-Yang <ejin1996@gmail.com>
2024-03-09 00:14:36 +0900

    refactor: 내비게이션바 개편

---
+++ src/components/Common/NavigationBar/navigationBar.css.ts
@@ -0,0 +1,34 @@
+import { style, styleVariants } from '@vanilla-extract/css';
+
+export const navigationBarContainer = style({
+  width: '100%',
+  height: 70,
+  paddingBottom: 6,
+});
+
+export const navigationList = style({
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  height: '100%',
+  borderTop: '1px solid #f9f9f9',
+});
+
+export const navigationLink = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 6,
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const menuNameBase = style({
+  fontSize: 11,
+  textAlign: 'center',
+  lineHeight: 1.4,
+});
+
+export const menuName = styleVariants({
+  active: [menuNameBase, { color: '#ffb017' }],
+  default: [menuNameBase, { color: '#a0a0a0' }],
+});
