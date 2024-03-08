38d1ec9b0507aee24d8ee65033c4247633312f2f
Leejin-Yang <ejin1996@gmail.com>
2024-03-09 02:07:37 +0900

    feat: 오늘의 리뷰 기본 이미지 설정

--- src/components/Rank/ReviewRankingItem/ReviewRankingItem.stories.tsx
+++ src/components/Rank/ReviewRankingItem/ReviewRankingItem.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import ReviewRankingItem from './ReviewRankingItem';
 
+import { REVIEW_CARD_DEFAULT_IMAGE_URL } from '@/constants/image';
+
 const meta: Meta<typeof ReviewRankingItem> = {
   title: 'review/ReviewRankingItem',
   component: ReviewRankingItem,
@@ -12,17 +14,17 @@ const meta: Meta<typeof ReviewRankingItem> = {
       productName: '구운감자슬림명란마요',
       content:
         '할머니가 먹을 거 같은 맛입니다. 1960년 전쟁 때 맛 보고 싶었는데 그때는 너무 가난해서 먹을 수 없었는데요 이것보다 긴 리뷰도 잘려 보인답니다',
-      image: 'https://via.placeholder.com/150',
+      image: REVIEW_CARD_DEFAULT_IMAGE_URL,
       categoryType: 'food',
       tags: [
         {
           id: 5,
-          name: '단짠단짠',
+          name: '😋 맛있어요',
           tagType: 'TASTE',
         },
         {
           id: 1,
-          name: '망고망고',
+          name: '🍭 달달해요',
           tagType: 'TASTE',
         },
       ],
---
+++ src/constants/image.ts
@@ -0,0 +1,2 @@
+const IMAGE_BASE_URL = 'https://image.funeat.site/prod/';
+export const REVIEW_CARD_DEFAULT_IMAGE_URL = `${IMAGE_BASE_URL}review-card-default.png`;
