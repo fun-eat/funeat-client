744cfcc5d24fa4f53dab3bf6bc4b9162a7facb10
TaeeunKim <xodms0309@naver.com>
2024-03-12 19:40:45 +0900

    fix: memberHandler에서 로그인 관련 로직 삭제

--- src/mocks/handlers/memberHandlers.ts
+++ src/mocks/handlers/memberHandlers.ts
@@ -5,31 +5,14 @@ import mockMemberReviews from '../data/memberReviews.json';
 
 export const memberHandlers = [
   rest.get('/api/members', (req, res, ctx) => {
-    const { mockSessionId } = req.cookies;
-
-    if (mockSessionId === 'abc123') {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          nickname: '냐미',
-          profileImage:
-            'https://github.com/woowacourse-teams/2023-fun-eat/assets/78616893/991f7b69-53bf-4d03-96e1-988c34d010ed',
-        })
-      );
-    }
-
-    if (mockSessionId === 'qwe456') {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          nickname: '뇨미',
-          profileImage:
-            'https://github.com/woowacourse-teams/2023-fun-eat/assets/78616893/84a20dae-4770-4497-ae25-2dd552261aab',
-        })
-      );
-    }
-
-    return res(ctx.status(401), ctx.json({ message: '로그인이 필요합니다.' }));
+    return res(
+      ctx.status(200),
+      ctx.json({
+        nickname: '냐미',
+        profileImage:
+          'https://github.com/woowacourse-teams/2023-fun-eat/assets/78616893/991f7b69-53bf-4d03-96e1-988c34d010ed',
+      })
+    );
   }),
 
   rest.put('/api/members', (req, res, ctx) => {
@@ -37,22 +20,10 @@ export const memberHandlers = [
   }),
 
   rest.get('/api/members/reviews', (req, res, ctx) => {
-    const { mockSessionId } = req.cookies;
-
-    if (!mockSessionId) {
-      return res(ctx.status(401), ctx.json({ message: '로그인이 필요합니다.' }));
-    }
-
     return res(ctx.status(200), ctx.json(mockMemberReviews));
   }),
 
   rest.get('/api/members/recipes', (req, res, ctx) => {
-    const { mockSessionId } = req.cookies;
-
-    if (!mockSessionId) {
-      return res(ctx.status(401), ctx.json({ message: '로그인이 필요합니다.' }));
-    }
-
     return res(ctx.status(200), ctx.json(mockMemberRecipes));
   }),
 
