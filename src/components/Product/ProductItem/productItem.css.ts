b290ae579c6cffcde4b67578a748de0b34f4915a
Leejin-Yang <ejin1996@gmail.com>
2024-03-13 15:10:51 +0900

    refactor: 상품 아이템 컴포넌트 디자인 개편

---
+++ src/components/Product/ProductItem/productItem.css.ts
@@ -0,0 +1,46 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  width: 163,
+});
+
+export const productImage = style({
+  border: '1px solid #e6e6e6',
+  borderRadius: '6px',
+  objectFit: 'cover',
+});
+
+export const productName = style({
+  fontSize: 16,
+  fontWeight: 600,
+  lineHeight: 1.4,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+});
+
+export const productPrice = style({
+  fontSize: 16,
+  fontWeight: 500,
+  lineHeight: 1.4,
+});
+
+export const summaryWrapper = style({
+  display: 'flex',
+  gap: 12,
+});
+
+export const previewWrapper = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: 4,
+});
+
+export const preview = style({
+  paddingTop: 2,
+  fontSize: 12,
+  fontWeight: 500,
+  lineHeight: 1.4,
+  color: '#999',
+});
--- src/types/product.ts
+++ src/types/product.ts
@@ -4,7 +4,7 @@ export interface Product {
   id: number;
   name: string;
   price: number;
-  image: string | null;
+  image: string;
   averageRating: number;
   reviewCount: number;
 }
