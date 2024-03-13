ecfe460b19a2e76f0d9f6e28871a3071f16899ae
Leejin Yang <ejin1996@gmail.com>
2024-03-13 16:51:03 +0900

    chore: eslint-plugin-unused-imports 추가 (#43)

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'react', 'import'],
+  plugins: ['@typescript-eslint', 'react', 'import', 'unused-imports'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-var-requires': 0,
@@ -103,6 +103,17 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/ban-types': 'off',
     'import/export': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
   settings: {
     'import/resolver': {
