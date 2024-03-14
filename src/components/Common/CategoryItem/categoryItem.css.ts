d0c55c7343719f89ce31be8d7d98279b4efa7565
Taeeun Kim <xodms0309@naver.com>
2024-03-14 16:21:34 +0900

    feat: 홈화면 카테고리 섹션 스타일 개편 (#34)
    * refactor: json 데이터값 이미지 변경
    
    * refactor: CategoryFoodList 컴포넌트 스타일 마이그레이션
    
    * refactor: CategoryItem 컴포넌트 스타일 마이그레이션
    
    * refactor: CategoryStoreList 컴포넌트 스타일 마이그레이션
    
    * refactor: Banner 컴포넌트 스타일 마이그레이션
    
    * refactor: Header 컴포넌트 스타일 마이그레이션
    
    * refactor: 홈페이지의 카테고리 섹션 스타일 마이그레이션
    
    * fix: 린트 오류 수정
    
    * feat: 홈화면에 검색화면으로 가는 버튼 추가
    
    * feat: SectionHeader 컴포넌트로 교체
    
    * feat: 카테고리 이름 옵셔널로 변경
    
    * feat: pages아래 HomePage 폴더 생성

---
+++ src/components/Common/CategoryItem/categoryItem.css.ts
@@ -0,0 +1,18 @@
+import { style } from '@vanilla-extract/css';
+
+export const imageWrapper = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '10px',
+});
+
+export const categoryImage = style({
+  objectFit: 'cover',
+});
+
+export const categoryName = style({
+  marginTop: 6,
+  fontSize: '1.2rem',
+  lineHeight: 1.4,
+});
---
+++ src/pages/HomePage/homePage.css.ts
@@ -0,0 +1,38 @@
+import { style } from '@vanilla-extract/css';
+
+export const sectionWrapper = style({
+  padding: '0 20px',
+});
+
+export const categorytSection = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 20,
+  padding: '20px 0 28px 0',
+  background: '#f2f2f2',
+});
+
+export const categoryListWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
+});
+
+export const rankingInfoWrapper = style({
+  display: 'flex',
+  gap: 2,
+  margin: '8px 0 16px',
+});
+
+export const searchRouterWrapper = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: 343,
+  height: 40,
+  padding: '10px 18px',
+  color: '#808080',
+  background: '#FFFFFF',
+  borderRadius: 20,
+  fontSize: '1.4rem',
+});
