2d1502ee58460df5399ca7a4b3ca2e17f07592c8
TaeeunKim <xodms0309@naver.com>
2024-03-18 20:24:22 +0900

    refactor: styleVariants를 사용하는 방식으로 변경

--- src/components/Common/Badge/Badge.tsx
+++ src/components/Common/Badge/Badge.tsx
@@ -1,6 +1,6 @@
 import type { ComponentPropsWithoutRef, CSSProperties } from 'react';
 
-import { outline, fill, text, outlinedBadge, filledBadge } from './badge.css';
+import { outline, fill, text, badge } from './badge.css';
 
 import { assignInlineVars } from '@vanilla-extract/dynamic';
 
@@ -13,7 +13,7 @@ export interface BadgeProps extends ComponentPropsWithoutRef<'div'> {
 const Badge = ({ outlined = false, color, textColor, children, ...props }: BadgeProps) => {
   return (
     <p
-      className={outlined ? outlinedBadge : filledBadge}
+      className={outlined ? badge['outlined'] : badge['filled']}
       style={assignInlineVars({
         [outline]: `1px solid ${color}`,
         [fill]: color,
--- src/components/Common/Badge/badge.css.ts
+++ src/components/Common/Badge/badge.css.ts
@@ -1,24 +1,19 @@
-import { createVar, style } from '@vanilla-extract/css';
+import { createVar, style, styleVariants } from '@vanilla-extract/css';
 
 export const outline = createVar();
 export const fill = createVar();
 export const text = createVar();
 
-const baseBadgeStyle = {
+const baseBadgeStyle = style({
   width: 'fit-content',
   padding: '5.5px 12px',
   fontSize: 12,
   color: text,
   borderRadius: 6,
   whiteSpace: 'nowrap',
-};
-
-export const outlinedBadge = style({
-  ...baseBadgeStyle,
-  border: outline,
 });
 
-export const filledBadge = style({
-  ...baseBadgeStyle,
-  background: fill,
+export const badge = styleVariants({
+  outlined: [baseBadgeStyle, { border: outline }],
+  filled: [baseBadgeStyle, { background: fill }],
 });
