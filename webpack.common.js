20e81a8a8ccbbdf1805a71070750d2d5b10f0d27
TaeeunKim <xodms0309@naver.com>
2024-03-18 20:00:31 +0900

    chore: webpack 설정 변경

--- webpack.common.js
+++ webpack.common.js
@@ -5,12 +5,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
-    clean: true,
-    publicPath: '/',
-  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
     alias: {
@@ -25,6 +19,9 @@ module.exports = {
         use: [
           {
             loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
           },
         ],
       },
