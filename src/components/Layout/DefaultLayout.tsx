c40967bc3aa742f1994802d0f261bc5e9be12cd3
Leejin-Yang <ejin1996@gmail.com>
2024-03-18 21:24:04 +0900

    refactor: 레이아웃 컴포넌트 수정

--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -6,25 +6,17 @@ import NavigationBar from '../Common/NavigationBar/NavigationBar';
 
 const DefaultLayout = ({ children }: PropsWithChildren) => {
   return (
-    <DefaultLayoutContainer>
+    <>
       <Header />
       <MainWrapper id="main">{children}</MainWrapper>
       <NavigationBar />
-    </DefaultLayoutContainer>
+    </>
   );
 };
 
 export default DefaultLayout;
 
-const DefaultLayoutContainer = styled.div`
-  height: 100%;
-  max-width: 600px;
-  margin: 0 auto;
-`;
-
 const MainWrapper = styled.main`
   position: relative;
-  height: calc(100% - 120px);
-  overflow-x: hidden;
-  overflow-y: auto;
+  padding: 50px 0 90px;
 `;
