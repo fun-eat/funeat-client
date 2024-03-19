c5dbebc4230901424d2ccc22e3ecb5cb78df4349
Leejin-Yang <ejin1996@gmail.com>
2024-03-20 03:10:10 +0900

    feat: 페이지 헤더 컴포넌트 스타일 수정

--- src/components/Common/PageHeader/pageHeader.css.ts
+++ src/components/Common/PageHeader/pageHeader.css.ts
@@ -1,11 +1,17 @@
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
+  position: 'fixed',
+  top: 0,
+  left: '50%',
   width: '100%',
+  maxWidth: 400,
   height: 50,
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
+  padding: '0 20px',
+  transform: 'translateX(-50%)',
 });
 
 export const headerTitle = style({
