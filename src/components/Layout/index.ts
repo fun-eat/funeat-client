e429d7e2efd133f74b777cd8e4eb17ce293588d9
Leejin Yang <ejin1996@gmail.com>
2024-03-19 22:12:56 +0900

    refactor: 레이아웃 개편 (#51)
    * refactor: 헤더, 내비게이션바 스타일 수정
    
    * refactor: 레이아웃 컴포넌트 수정
    
    * refactor: 루트 스타일 수정
    
    * refactor: 헤더 컴포넌트 프롭 삭제
    
    * feat: 홈페이지 스타일 수정
    
    * refactor: 홈페이지 리스트 패딩 추가
    
    * feat: 스크롤바 숨기는 스타일 적용
    
    * refactor: 레이아웃 컴포넌트 스타일 마이그레이션
    
    * refactor: 헤더 클래스네임 수정
    
    * refactor: relative 삭제
    
    * refactor: 앱 높이 수정
    
    * refactor: 스타일 수정
    
    * feat: 내비게이션 아이콘 스타일 수정

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,5 +1,3 @@
 export { default as DefaultLayout } from './DefaultLayout';
 export { default as MinimalLayout } from './MinimalLayout';
-export { default as HeaderOnlyLayout } from './HeaderOnlyLayout';
 export { default as AuthLayout } from './AuthLayout';
-export { default as SimpleHeaderLayout } from './SimpleHeaderLayout';
