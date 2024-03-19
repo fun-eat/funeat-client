a35e31aa7e672a54a4f4ae37f33db0216a242889
hae-on <solbi2004@naver.com>
2024-03-19 13:53:19 +0900

    refactor: 스크롤 화살표 색상 적용

--- src/components/Common/ScrollButton/ScrollButton.tsx
+++ src/components/Common/ScrollButton/ScrollButton.tsx
@@ -21,7 +21,7 @@ const ScrollButton = ({ targetRef }: ScrollButtonProps) => {
 
   return (
     <button className={container} onClick={handleScroll}>
-      <SvgIcon variant="arrowUp" width={17} height={17} />
+      <SvgIcon variant="arrowUp" width={17} height={17} stroke="#6B6B6B" />
     </button>
   );
 };
