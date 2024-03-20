4034d7a14739f202ee845d2528fc3f6e48be3f30
Leejin-Yang <ejin1996@gmail.com>
2024-03-20 23:58:05 +0900

    feat: 메인 높이 수정

--- src/components/Layout/layout.css.ts
+++ src/components/Layout/layout.css.ts
@@ -1,11 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
 export const defaultLayout = style({
-  height: '100%',
   padding: '50px 0 70px',
 });
 
 export const minimalLayout = style({
-  height: '100%',
   paddingTop: 50,
 });
