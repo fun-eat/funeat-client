b6ce68998c59bc6599e087bf72d0c1c5e1d52b3e
hae-on <solbi2004@naver.com>
2024-03-27 00:18:51 +0900

    feat: recipeProductButton 구현

---
+++ src/components/Recipe/RecipeProductButton/RecipeProductButton.tsx
@@ -0,0 +1,18 @@
+import cx from 'classnames';
+
+import { SvgIcon } from '@/components/Common';
+import { container, translucent } from './recipeProductButton.css';
+
+interface RecipeProductButtonProps {
+  isTranslucent?: boolean;
+}
+
+const RecipeProductButton = ({ isTranslucent }: RecipeProductButtonProps) => {
+  return (
+    <button className={cx(container, { [translucent]: isTranslucent })}>
+      <SvgIcon variant="disk" stroke="white" fill="none" width={12} height={12} />
+    </button>
+  );
+};
+
+export default RecipeProductButton;
---
+++ src/components/Recipe/RecipeProductButton/recipeProductButton.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  width: 26,
+  height: 26,
+  background: '#D9D9D9',
+  borderRadius: '50%',
+});
+
+export const translucent = style({
+  opacity: '50%',
+});
