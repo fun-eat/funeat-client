a3922e84530793b4056cd14660066e8e1ad9ff68
TaeeunKim <xodms0309@naver.com>
2024-03-31 23:02:31 +0200

    feat: ProductSearchResultList 스토리북 추가

---
+++ src/components/Search/ProductSearchResultList/ProductSearchResultList.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ProductSearchResultList from './ProductSearchResultList';
+
+const meta: Meta<typeof ProductSearchResultList> = {
+  title: 'search/ProductSearchResultList',
+  component: ProductSearchResultList,
+  args: {
+    searchQuery: '꼬북칩',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
