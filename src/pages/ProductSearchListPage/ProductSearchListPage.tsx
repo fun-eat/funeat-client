5a0896969d18e47e3422cf80cb2ea69b5185a51b
TaeeunKim <xodms0309@naver.com>
2024-04-08 21:13:12 +0200

    feat: ProductOverviewList로 교체

--- src/pages/ProductSearchListPage/ProductSearchListPage.tsx
+++ src/pages/ProductSearchListPage/ProductSearchListPage.tsx
@@ -1,11 +1,10 @@
 import { useRef } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 import { container } from './productSearchListPage.css';
 
 import { PageHeader } from '@/components/Common';
-import { ProductOverviewItem } from '@/components/Product';
-import { PATH } from '@/constants/path';
+import { ProductOverviewList } from '@/components/Product';
 import { useIntersectionObserver } from '@/hooks/common';
 import { useInfiniteProductSearchResultsQuery } from '@/hooks/queries/search';
 
@@ -22,18 +21,9 @@ export const ProductSearchListPage = () => {
   return (
     <>
       <PageHeader title={`'${searchQuery}'이/가 포함된 상품`} hasBackLink />
-      <ul className={container}>
-        {products.map(({ id, categoryType, image, name, price, averageRating }) => (
-          <li key={id}>
-            <Link to={`${PATH.PRODUCT_LIST}/${categoryType}/${id}`}>
-              <ProductOverviewItem image={image} name={name} price={price} rate={averageRating} />
-            </Link>
-            <div style={{ height: '20px' }} />
-            <hr style={{ border: '0.5px solid #e6e6e6' }} />
-            <div style={{ height: '20px' }} />
-          </li>
-        ))}
-      </ul>
+      <div className={container}>
+        <ProductOverviewList products={products} isSearchPage />
+      </div>
       <div ref={scrollRef} aria-hidden />
     </>
   );
