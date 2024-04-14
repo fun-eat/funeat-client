c6e707f94591e340bb9476398dd83a5c01e96ad4
Leejin-Yang <ejin1996@gmail.com>
2024-04-14 14:24:34 +0200

    feat: 이미지 업로더 스타일 개편

--- src/components/Common/ImageUploader/imageUploader.css.ts
+++ src/components/Common/ImageUploader/imageUploader.css.ts
@@ -1,17 +1,56 @@
 import { vars } from '@/styles/theme.css';
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 
-export const uploadLabel = style({
+export const container = style({
   display: 'flex',
+  gap: 8,
+});
+
+export const uploadLabelBase = style({
+  display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
   width: 80,
   height: 80,
-  backgroundColor: vars.colors.background.category,
   borderRadius: 6,
   cursor: 'pointer',
 });
 
+export const uploadLabel = styleVariants({
+  default: [uploadLabelBase, { backgroundColor: vars.colors.background.category }],
+  uploaded: [
+    uploadLabelBase,
+    { backgroundColor: vars.colors.background.default, border: `1px solid ${vars.colors.border.default}` },
+  ],
+});
+
 export const uploadInput = style({
   display: 'none',
+});
+
+export const imageWrapper = style({
+  position: 'relative',
+  width: 80,
+  height: 80,
+});
+
+export const image = style({
+  objectFit: 'cover',
+  borderRadius: 6,
+});
+
+export const deleteButton = style({
+  position: 'absolute',
+  top: 4,
+  right: 4,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: 18,
+  height: 18,
+  borderRadius: '50%',
+  backgroundColor: vars.colors.black,
+  opacity: 0.5,
+  cursor: 'pointer',
 });
