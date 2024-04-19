c128a7cd2435377860478a7397fc2ddc813c98ee
Leejin-Yang <ejin1996@gmail.com>
2024-04-19 21:49:39 +0200

    feat: 인디케이터 추가

--- src/components/Common/NavigationBar/navigationBar.css.ts
+++ src/components/Common/NavigationBar/navigationBar.css.ts
@@ -1,3 +1,4 @@
+import { vars } from '@/styles/theme.css';
 import { style, styleVariants } from '@vanilla-extract/css';
 
 export const container = style({
@@ -6,17 +7,16 @@ export const container = style({
   left: '50%',
   width: '100%',
   maxWidth: 400,
-  height: 70,
-  backgroundColor: '#fff',
+  backgroundColor: vars.colors.white,
   transform: 'translateX(-50%)',
 });
 
 export const list = style({
   display: 'flex',
   justifyContent: 'space-around',
-  alignItems: 'center',
-  height: '100%',
-  border: '1px solid #f9f9f9',
+  alignItems: 'flex-end',
+  height: 60,
+  borderTop: `1px solid ${vars.colors.border.light}`,
 });
 
 export const link = style({
@@ -30,10 +30,9 @@ export const link = style({
 const menuNameBase = style({
   fontSize: 11,
   textAlign: 'center',
-  lineHeight: 1.4,
 });
 
 export const menuName = styleVariants({
-  active: [menuNameBase, { color: '#ffb017' }],
-  default: [menuNameBase, { color: '#a0a0a0' }],
+  active: [menuNameBase, { color: vars.colors.primary }],
+  default: [menuNameBase, { color: vars.colors.gray3 }],
 });
