4761dfbcb718df0a2101656fe2815629c3b8c3a5
hae-on <solbi2004@naver.com>
2024-04-20 15:29:35 +0900

    feat: 작성한 꿀조합/리뷰 클릭에 따른 페이지 이동 구현

--- src/components/Members/PostCounterBox/PostCounterBox.tsx
+++ src/components/Members/PostCounterBox/PostCounterBox.tsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import { border, box, container } from './postCounterBox.css';
 
 import { Text } from '@/components/Common';
+import { PATH } from '@/constants/path';
 
 interface PostCounterBoxProps {
   recipeCount: number;
@@ -10,23 +12,27 @@ interface PostCounterBoxProps {
 const PostCounterBox = ({ recipeCount, reviewCount }: PostCounterBoxProps) => {
   return (
     <div className={container}>
-      <div className={box}>
-        <Text size="caption1" weight="medium" color="disabled">
-          작성한 꿀조합
-        </Text>
-        <Text size="headline" weight="medium" color="sub">
-          {recipeCount}개
-        </Text>
-      </div>
+      <Link to={`${PATH.MEMBER}/post`} state="recipe">
+        <div className={box}>
+          <Text size="caption1" weight="medium" color="disabled">
+            작성한 꿀조합
+          </Text>
+          <Text size="headline" weight="medium" color="sub">
+            {recipeCount}개
+          </Text>
+        </div>
+      </Link>
       <div className={border} />
-      <div className={box}>
-        <Text size="caption1" weight="medium" color="disabled">
-          작성한 리뷰
-        </Text>
-        <Text size="headline" weight="medium" color="sub">
-          {reviewCount}개
-        </Text>
-      </div>
+      <Link to={`${PATH.MEMBER}/post`} state="review">
+        <div className={box}>
+          <Text size="caption1" weight="medium" color="disabled">
+            작성한 리뷰
+          </Text>
+          <Text size="headline" weight="medium" color="sub">
+            {reviewCount}개
+          </Text>
+        </div>
+      </Link>
     </div>
   );
 };
