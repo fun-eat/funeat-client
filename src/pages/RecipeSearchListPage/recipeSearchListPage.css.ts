388c8d19ec91393293d9a912a55327a4c0886168
Leejin-Yang <ejin1996@gmail.com>
2024-04-20 09:55:43 +0200

    feat: 검색 페이지 레이아웃 수정

--- src/pages/RecipeSearchListPage/recipeSearchListPage.css.ts
+++ src/pages/RecipeSearchListPage/recipeSearchListPage.css.ts
@@ -1,5 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
+export const main = style({
+  paddingTop: 50,
+});
+
 export const listWrapper = style({
   display: 'grid',
   gridTemplateColumns: '1fr 1fr',
