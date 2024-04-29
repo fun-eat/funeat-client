abb5d563a982514349da50562f48a5302882e9b2
Taeeun Kim <xodms0309@naver.com>
2024-04-29 17:38:42 +0200

    fix: 조합실 리스트 grid 레이아웃 깨지는 오류 해결 (#120)

--- src/components/Recipe/RecipeList/recipeList.css.ts
+++ src/components/Recipe/RecipeList/recipeList.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const container = style({
   display: 'grid',
-  gridTemplateColumns: 'minmax(0, 1fr) 1fr',
+  gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
   gridAutoRows: 'auto',
   columnGap: 10,
   rowGap: 20,
