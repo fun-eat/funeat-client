3ef9316e736431e38d656b2e3d2abc7f762d4685
hae-on <solbi2004@naver.com>
2024-04-30 15:41:41 +0900

    refactor: 사용하지 않는 컴포넌트 삭제

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -5,7 +5,6 @@ export { default as SvgSprite } from './Svg/SvgSprite';
 export { default as SvgIcon } from './Svg/SvgIcon';
 export { default as TabMenu } from './TabMenu/TabMenu';
 export { default as TagList } from './TagList/TagList';
-export { default as SectionTitle } from './SectionTitle/SectionTitle';
 export { default as ScrollButton } from './ScrollButton/ScrollButton';
 export { default as ImageUploader } from './ImageUploader/ImageUploader';
 export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
