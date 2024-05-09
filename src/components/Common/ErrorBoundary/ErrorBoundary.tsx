e5f333867fed5d3d548b24cb068332b98f8ef3e9
hae-on <solbi2004@naver.com>
2024-05-09 22:36:09 +0900

    refactor: 에러 바운더리 임시 디자인 수정

--- src/components/Common/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/Common/ErrorBoundary/ErrorBoundary.tsx
@@ -1,6 +1,8 @@
-import { Button } from '@fun-eat/design-system';
 import type { ComponentType, PropsWithChildren } from 'react';
 import { Component } from 'react';
+
+import { button, buttonWrapper } from './errorBoundary.css';
+import Text from '../Text/Text';
 
 export interface FallbackProps {
   message: string;
@@ -39,9 +41,14 @@ class ErrorBoundary extends Component<PropsWithChildren<ErrorBoundaryProps>, Err
       return (
         <>
           <FallbackComponent message={this.state.error.message} />
-          <Button type="button" onClick={this.resetError}>
-            다시 시도하기
-          </Button>
+          <div className={buttonWrapper}>
+            <button type="button" className={button} onClick={this.resetError}>
+              <Text size="caption1" weight="semiBold" color="info">
+                다시 시도하기
+              </Text>
+            </button>
+          </div>
+          <div style={{ height: 10 }} />
         </>
       );
     }
--- src/components/Common/ErrorComponent/ErrorComponent.tsx
+++ src/components/Common/ErrorComponent/ErrorComponent.tsx
@@ -1,5 +1,20 @@
+import { container } from './errorComponent.css';
+import Text from '../Text/Text';
+
+import NotFound from '@/assets/not-found.png';
+
 const ErrorComponent = () => {
-  return <div>에러가 발생했습니다.</div>;
+  return (
+    <div className={container}>
+      <div style={{ height: 10 }} />
+      <img src={NotFound} alt="404 캐릭터" width={83} height={77} />
+      <div style={{ height: 24 }} />
+      <Text size="headline" weight="semiBold" color="sub">
+        에러가 발생했습니다
+      </Text>
+      <div style={{ height: 20 }} />
+    </div>
+  );
 };
 
 export default ErrorComponent;
