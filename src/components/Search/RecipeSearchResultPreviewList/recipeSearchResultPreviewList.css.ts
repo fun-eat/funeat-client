67027bb2c8a658aacfac43e6fc17fce6689e4069
hae-on <solbi2004@naver.com>
2024-07-21 01:22:58 +0900

    refactor: 검색창 꿀조합 상품 하나일 때 커지는 현상 방지

--- src/components/Search/RecipeSearchResultPreviewList/recipeSearchResultPreviewList.css.ts
+++ src/components/Search/RecipeSearchResultPreviewList/recipeSearchResultPreviewList.css.ts
@@ -1,8 +1,14 @@
 import { style } from '@vanilla-extract/css';
 
-export const listWrapper = style({
+export const container = style({
   display: 'flex',
   gap: 10,
   alignItems: 'center',
   overflowY: 'scroll',
+});
+
+export const wrapper = style({
+  height: '100%',
+  display: 'flex',
+  flex: '0 0 160px',
 });
