059f4f055be1eb527c5f6c3f1602ac638fcb8022
hae-on <solbi2004@naver.com>
2025-11-23 12:40:45 +0900

    refact: 이미지 public으로 경로 수정하고 json에서 이미지 가져오도록 개선

--- src/mocks/handlers/bannerHandlers.ts
+++ src/mocks/handlers/bannerHandlers.ts
@@ -1,20 +1,6 @@
 import { rest } from 'msw';
 
-import banner1 from '@/assets/banner1.png';
-import banner2 from '@/assets/banner2.png';
-
-const banners = [
-  {
-    id: 2,
-    link: '/products',
-    image: banner2,
-  },
-  {
-    id: 1,
-    link: '/recipes',
-    image: banner1,
-  },
-];
+import banners from '../data/banners.json';
 
 export const bannerHandlers = [
   rest.get('/api/banners', (req, res, ctx) => {
